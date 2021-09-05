74c883b3b21b0dc7b3023ae72f8402f2a34a2a32
Emilio Conde <condeemilio891@gmail.com>
2021-09-04 21:51:13 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import NavBar from "./Components/NavBar"
+import {BrowserRouter as Router} from "react-router-dom"
+
+
+
+
+
+function App() {
+  return (
+
+    <Router>
+      <NavBar/>
+    </Router>
+    
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import { Nav, NavbarContainer, NavLogo, MobileIcon,NavMenu,NavItem,NavLinks } from './navbarElements'
+
+const Navbar = () => {
+    return (
+        <>
+
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to="/">
+                    denim haircut
+                </NavLogo>
+                <MobileIcon>
+                    <FaBars/>
+                </MobileIcon>
+                <NavMenu>
+
+                    <NavItem>
+                        <NavLinks to="about">About
+
+                        </NavLinks>
+                    </NavItem>
+                </NavMenu>
+            </NavbarContainer>
+        </Nav>
+        </>
+    )
+}
+
+export default Navbar
