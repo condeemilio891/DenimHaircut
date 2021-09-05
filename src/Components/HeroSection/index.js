98696fd6d11653005d71177b594660d1d739fa26
Emilio Conde <condeemilio891@gmail.com>
2021-09-05 13:02:34 -0500

    added styling into hero section

--- src/Components/HeroSection/index.js
+++ src/Components/HeroSection/index.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import Home from '../../pages/bandPage/bandPage'
+import React,{useState} from 'react'
+import { Button } from '../ButtonElement'
 import Video from "../../assets/videos/GirlDealing.mp4"
-import {HeroContainer, HeroBg,VideoBg} from "../HeroSection/heroElements"
+import {HeroContainer, HeroBg,VideoBg, HeroContent,HeroH1,HeroP,HeroBtnWrapper,ArrowForward,ArrowRight} from "../HeroSection/heroElements"
 
 
 const HeroSection = () => {
+
+    const[hover,setHover]= useState(false)
+
+    const onHover=()=>{
+        setHover(!hover)
+    }
     return (
         <HeroContainer id='home'>
             <HeroBg>
@@ -12,10 +18,14 @@ const HeroSection = () => {
                 mp4'/>
             </HeroBg>
             <HeroContent>
-                <HeroH1></HeroH1>
-                <HeroP></HeroP>
+                <HeroH1>Denium Haircut</HeroH1>
+                <HeroP>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem 
+                    sequi nesciunt. Neque porro quisquam</HeroP>
                 <HeroBtnWrapper>
-                    <Button to='signup'>
+                    <Button to='signup'onMouseEnter={onHover}
+                    onMouseLeave={onHover}
+                    primary='true'
+                    dark='true'>
                         Get Started {hover ?<ArrowForward/>:<ArrowRight/>}
                     </Button>
                 </HeroBtnWrapper>
