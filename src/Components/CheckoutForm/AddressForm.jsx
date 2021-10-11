f9a82d357f527f0f4d515ebbec7d635546780437
Emilio Conde <condeemilio891@gmail.com>
2021-10-11 11:19:33 -0500

    checkout rendering now on to confirmation and testing

--- src/Components/CheckoutForm/AddressForm.jsx
+++ src/Components/CheckoutForm/AddressForm.jsx
@@ -47,31 +47,29 @@ const AddressForm = ({checkoutToken, next,nextStep,test}) => {
         if (shippingSubdivision) fetchShippingOptions(checkoutToken.id, shippingCountry, shippingSubdivision);
       }, [shippingSubdivision]);
     return (
-        <>
-            <Typography varient="h6" gutterBottom> Shipping Address</Typography>
-            <FormProvider {...methods}>
-                <form onSubmit={methods.handleSubmit((data)=>test({...data,shippingCountry,shippingSubdivision,setShippingOption}))}>
-                    <Grid container spacing={3}>
-                        <FormInput required name='firstName' label='First name'/>
-                        <FormInput required name='lastName' label='Last name'/>
-                        <FormInput required name='address1' label='Address'/>
-                        <FormInput required name='email' label='Email'/>
-                        <FormInput required name='city' label='City'/>
-                        <FormInput required name='zip' label='Zip/postal Code'/>
-
-
-                        <Grid item xs={12} sm={6}>
-                            <InputLabel>Shipping Country</InputLabel>
-                                <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
-                                    {Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name })).map((item) => (
-                                     <MenuItem key={item.id} value={item.id}>
-                                            {item.label}
-                                        </MenuItem>
-                                         ))}
-                                </Select>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-              <InputLabel></InputLabel>
+      <>
+      <Typography variant="h6" gutterBottom>Shipping address</Typography>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit((data) => test({ ...data, shippingCountry, shippingSubdivision, shippingOption }))}>
+          <Grid container spacing={3}>
+            <FormInput required name="firstName" label="First name" />
+            <FormInput required name="lastName" label="Last name" />
+            <FormInput required name="address1" label="Address line 1" />
+            <FormInput required name="email" label="Email" />
+            <FormInput required name="city" label="City" />
+            <FormInput required name="zip" label="Zip / Postal code" />
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+                {Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name })).map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Subdivision</InputLabel>
               <Select value={shippingSubdivision} fullWidth onChange={(e) => setShippingSubdivision(e.target.value)}>
                 {Object.entries(shippingSubdivisions).map(([code, name]) => ({ id: code, label: name })).map((item) => (
                   <MenuItem key={item.id} value={item.id}>
@@ -90,16 +88,15 @@ const AddressForm = ({checkoutToken, next,nextStep,test}) => {
                 ))}
               </Select>
             </Grid>
-                    </Grid>
-
-                    <Button type="submit" varient="contained" color="primary">Next</Button>
-                </form>
-                <br/>
-                <div style={{display:'flex', justifyContent:"space-between"}}></div>
-                    <Button component={Link} to="/store/cart" varient="outlined">Back to Cart</Button>
-                   
-            </FormProvider>
-        </>
+          </Grid>
+          <br />
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Button component={Link} variant="outlined" to="/cart">Back to Cart</Button>
+            <Button type="submit" variant="contained" color="primary">Next</Button>
+          </div>
+        </form>
+      </FormProvider>
+    </>
     )
 }
 
--- src/Components/CheckoutForm/PaymentForm.jsx
+++ src/Components/CheckoutForm/PaymentForm.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import {Typography, Button, Divider} from "@material-ui/core"
-import {Elements, CardElement,ElementConsumer} from "@stripe/react-stripe-js"
+import {Elements, CardElement,ElementsConsumer} from "@stripe/react-stripe-js"
 import { loadStripe  } from '@stripe/stripe-js'
 import Review from './Review'
 
@@ -42,27 +42,26 @@ const handleSubmit= async (event,elements,stripe)=>{
     return (
         
 
-        <>
-            <Review checkoutToken={checkoutToken}/>
-            <Divider/>
-            <Typography varient ="h6" gutterBottom style={{margin:"20px 0"}}>Payment Method</Typography>
-            <Elements stripe={stripePromise}>
-                <ElementConsumer>
-                    {({elements,stripe})=>(
-                        <form onSubmit={(e)=> handleSubmit(e,elements,stripe)}>
-                            <CardElement/>
-                            <br/><br/>
-                            <div style={{display:'flex', justifyContent:"space-between"}}>
-                                <Button varient="outlined" onClick={backStep}>Back</Button>
-                                <Button type="submit" varient="contained" disabled={!stripe} color="primary">
-                                    Pay {checkoutToken.live.subtotal}
-                                </Button>
-                            </div>
-                        </form>
-                    )}
-                </ElementConsumer>
-            </Elements>
-        </>
+      <>
+      <Review checkoutToken={checkoutToken} />
+      <Divider />
+      <Typography variant="h6" gutterBottom style={{ margin: '20px 0' }}>Payment method</Typography>
+      <Elements stripe={stripePromise}>
+        <ElementsConsumer>{({ elements, stripe }) => (
+          <form onSubmit={(e) => handleSubmit(e, elements, stripe)}>
+            <CardElement />
+            <br /> <br />
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button variant="outlined" onClick={backStep}>Back</Button>
+              <Button type="submit" variant="contained" disabled={!stripe} color="primary">
+                Pay {checkoutToken.live.subtotal.formatted_with_symbol}
+              </Button>
+            </div>
+          </form>
+        )}
+        </ElementsConsumer>
+      </Elements>
+    </>
     )
 }
 
